0207bcfc244789abefb5835b28d5e4a2eb74a728
Harllem Nascimento <harllemnascimento@gmail.com>
2019-08-21 21:32:08 -0300

    Lista 01

---
+++ lista01ex05.py
@@ -0,0 +1,5 @@
+print('Conversão Monetária: Real para Dolar!')
+Real = float(input('Insira quanto em "R$" você deseja converter: '))
+Dolar = float(input('Insira a cotação atual do Dolar: '))
+Conversao = Real * Dolar
+print('Você irá obter US${}!! '.format(Conversao))
