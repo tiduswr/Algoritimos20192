101e5101f96082090c0433b756498b0aacbd8086
Harllem Nascimento <harllemnascimento@gmail.com>
2019-08-22 21:48:24 -0300

    Lista 01

--- Exemplo Triangulos 22-08-2019.py
+++ Exemplo Triangulos 22-08-2019.py
@@ -5,7 +5,7 @@ c = int(input('Terceiro lado: '))
 if a + b <= c or b + c <= a or a + c <= b:
 	print('Os valores informados não formam um triângulo! ')
 elif a == b and b == c:
-	print('Triangulo escaleno!')
+	print('Triangulo Equilatero!')
 elif a != b and b != c and a != c:
 	print('Triangulo escaleno!')
 else:
