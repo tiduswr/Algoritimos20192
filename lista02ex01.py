20176f51a5623b63561a2a21098af9ea468f6e58
Harllem Nascimento <harllemnascimento@gmail.com>
2019-08-23 22:08:30 -0300

    Lista 02

--- lista02ex01.py
+++ lista02ex01.py
@@ -1,4 +1,4 @@
-sexo = str(input('Digite seu sexo, utilizando (1) Masculino ou (2) Feminino: '))
+sexo = int(input('Digite seu sexo, utilizando (1) Masculino ou (2) Feminino: '))
 if sexo == '1':
     print('Sexo Masculino.')
 elif sexo == '2':
---
+++ lista02ex02.py
@@ -0,0 +1,6 @@
+print('Verificado de Vogal ou consoante!')
+cond = str(input('Deigite uma letra: '))
+if cond == 'a' or cond == 'e' or cond == 'i' or cond == 'o' or cond == 'u':
+    print('A letra digitada é uma vogal! ')
+else:
+    print('A letra digitada é uma consoante! ')
