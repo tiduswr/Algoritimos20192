13c1ecf56c53188b56470952f081e6bcef449c99
Harllem Nascimento <harllemnascimento@gmail.com>
2019-08-23 21:25:19 -0300

    Lista 01

--- lista01ex10.py
+++ lista01ex10.py
@@ -6,6 +6,6 @@ comcad = float(input('Digite o comprimento de uma das cadeira: '))
 larcad = float(input('Digite a largura de uma das cadeiras: '))
 mcad = comcad * larcad
 mareaprof = msala - 1.5
-mareacad = mcad + 0.7
+mareacad = mcad + (0.7 * 0.2)
 cadtotal = mareaprof / mareacad
-print('Você conseguira orgabizar um total de {:.0f} \n cadeira no espaço dessa sala de aula!'.format(cadtotal))
+print('Você conseguira organizar um total de {:.0f} \n cadeira no espaço dessa sala de aula!'.format(cadtotal))
