b20c10d8a80b0aafeb4a38e0aa7b11a72c46349e
Harllem Nascimento <harllemnascimento@gmail.com>
2019-08-23 21:59:21 -0300

    Lista 02

---
+++ lista02ex01.py
@@ -0,0 +1,7 @@
+sexo = str(input('Digite seu sexo, utilizando (1) Masculino ou (2) Feminino: '))
+if sexo == '1':
+    print('Sexo Masculino.')
+elif sexo == '2':
+    print('Sexo Feminino.')
+else:
+    print('Sexo Invalido!')
