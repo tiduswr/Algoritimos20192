164235bab68baed82d05063fa41dfca62bc78f21
Harllem Nascimento <54108636+tiduswr@users.noreply.github.com>
2019-08-28 09:53:02 -0300

    Update lista02ex02.py

--- lista02ex02.py
+++ lista02ex02.py
@@ -1,6 +1,8 @@
-print('Verificado de Vogal ou consoante!')
+print('Verificador de Vogal ou consoante!')
 cond = str(input('Deigite uma letra: '))
-if cond == 'a' or cond == 'e' or cond == 'i' or cond == 'o' or cond == 'u':
+if cond == 'a' or cond == 'e' or cond == 'i' or cond == 'o' or cond == 'u' or cond == 'A' or cond == 'E' or cond == 'I' or cond == 'O' or cond == 'U':
     print('A letra digitada é uma vogal! ')
+elif cond == 'b' or cond == 'c' or cond == 'd' or cond == 'f' or cond == 'g' or cond == 'h' or cond == 'j' or cond == 'k' or cond == 'l' or cond == 'm' or cond == 'n' or cond == 'p' or cond == 'q' or cond == 'r' or cond == 's' or cond == 't' or cond == 'v' or cond == 'x' or cond == 'z' or cond == 'w' or cond == 'y' or cond == 'B' or cond == 'C' or cond == 'D' or cond == 'F' or cond == 'G' or cond == 'H' or cond == 'J' or cond == 'K' or cond == 'L' or cond == 'M' or cond == 'N' or cond == 'P' or cond == 'Q' or cond == 'R' or cond == 'S' or cond == 'T' or cond == 'V' or cond == 'X' or cond == 'Z' or cond == 'W' or cond == 'Y':
+	print('A letra digitada é uma consoante. ')
 else:
-    print('A letra digitada é uma consoante! ')
+	print('Você não digitou uma letra!! '
