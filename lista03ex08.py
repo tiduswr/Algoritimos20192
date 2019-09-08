77b3b4e010757c282ff9c6a0029c7f282ca7a7d8
Harllem Nascimento <54108636+tiduswr@users.noreply.github.com>
2019-09-07 22:28:16 -0300

    Lista 03

---
+++ lista03ex08.py
@@ -0,0 +1,6 @@
+print('Saiba os valores compreendidos entre os numeros\ninteiros digitados:\n')
+num1 = int(input('Insira um numero: '))
+num2 = int(input('Insira outro numero: '))
+print('')
+for i in range(num1+1,num2,1):
+	print(i)
