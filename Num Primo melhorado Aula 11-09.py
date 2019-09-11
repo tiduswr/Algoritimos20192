a02640110ea93533598aa98561f01f9b37eb9c1a
Harllem Nascimento <54108636+tiduswr@users.noreply.github.com>
2019-09-11 09:39:26 -0300

    Aula 11-09-2019

---
+++ Num Primo melhorado Aula 11-09.py
@@ -0,0 +1,15 @@
+#Imprima os N primeiros numeros primos onde N é informado pelo Usuario!
+quant = int(input('Quantos Primos? '))
+print(1)
+numero = 2
+cont = 1
+while cont < quant:
+	primo = True
+	for i in range(2,numero,1):
+		if numero % i ==0:
+			primo = False
+	if primo:
+		print(numero)
+		cont = cont + 1
+	numero = numero + 1
+			
